96ffe3e44b58a8e79e8d37f9871b407388e678b0
TamLe2001 <TamLe2001@live.dk>
2026-02-05 12:58:35 +0100

    added published bool for outfits for explore page (#1)

--- api/models/outfit.py
+++ api/models/outfit.py
@@ -14,6 +14,7 @@ def normalize_outfit_payload(payload):
 		'shirt': payload.get('shirt'),
 		'pants': payload.get('pants'),
         'shoes': payload.get('shoes'),
+		'published': payload.get('published', False),
 		'created_at': payload.get('created_at') or datetime.now(timezone.utc),
 	}
 
@@ -26,10 +27,12 @@ def serialize_outfit(outfit_doc):
 		'id': str(outfit_doc.get('_id')),
 		'name': outfit_doc.get('name'),
 		'user_id': outfit_doc.get('user_id'),
+		'style': outfit_doc.get('style'),
 		'bio': outfit_doc.get('bio'),
 		'hat': outfit_doc.get('hat'),
 		'shirt': outfit_doc.get('shirt'),
 		'pants': outfit_doc.get('pants'),
         'shoes': outfit_doc.get('shoes'),
+        'published': outfit_doc.get('published', False),
 		'created_at': outfit_doc.get('created_at'),
 	}
