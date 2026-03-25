97498a3de091c2ec8fb88de088eaeba437862497
Kenny <TamLe2001@live.dk>
2026-03-25 10:55:17 +0100

    fix problem

--- seed.py
+++ seed.py
@@ -281,7 +281,7 @@ def _seed_default_garments(db):
 
     for garment in all_garments:
         db.garments.update_one(
-            {"id": garment["id"]},
+            {"_id": garment["_id"]},
             {
                 "$set": {
                     **garment,
