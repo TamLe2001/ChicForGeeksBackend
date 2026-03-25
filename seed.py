45363fd7ce8a80a69ec2094fb16edd34f98a4b98
Kenny <TamLe2001@live.dk>
2026-03-25 10:25:44 +0100

    change id

--- seed.py
+++ seed.py
@@ -14,7 +14,7 @@ def _seed_default_garments(db):
     shirts = [
         {
             "type": "shirt",
-            "id": "tshirt_fitted_black_female",
+            "_id": "tshirt_fitted_black_female",
             "name": "tshirt_fitted_black_female.glb",
             "display_name": "Fitted Black Tshirt",
             "user_id": "default",
@@ -24,7 +24,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_fitted_white_female",
+            "_id": "tshirt_fitted_white_female",
             "name": "tshirt_fitted_white_female.glb",
             "display_name": "Fitted White Tshirt",
             "user_id": "default",
@@ -34,7 +34,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_longsleeve_black_female",
+            "_id": "tshirt_longsleeve_black_female",
             "name": "tshirt_longsleeve_black_female.glb",
             "display_name": "Black Longsleeve",
             "user_id": "default",
@@ -44,7 +44,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_longsleeve_white_female",
+            "_id": "tshirt_longsleeve_white_female",
             "name": "tshirt_longsleeve_white_female.glb",
             "display_name": "White Longsleeve",
             "user_id": "default",
@@ -54,7 +54,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_white_male",
+            "_id": "tshirt_white_male",
             "name": "tshirt_white_male.glb",
             "display_name": "White Tshirt",
             "user_id": "default",
@@ -64,7 +64,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_black_male",
+            "_id": "tshirt_black_male",
             "name": "tshirt_black_male.glb",
             "display_name": "Black Tshirt",
             "user_id": "default",
@@ -74,7 +74,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_longsleeve_white_male",
+            "_id": "tshirt_longsleeve_white_male",
             "name": "tshirt_longsleeve_white_male.glb",
             "display_name": "White Longsleeve",
             "user_id": "default",
@@ -84,7 +84,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "shirt",
-            "id": "tshirt_longsleeve_black_male",
+            "_id": "tshirt_longsleeve_black_male",
             "name": "tshirt_longsleeve_black_male.glb",
             "display_name": "Black Longsleeve",
             "user_id": "default",
@@ -98,7 +98,7 @@ def _seed_default_garments(db):
     pants = [
         {
             "type": "pants",
-            "id": "pants_baggy_black_female",
+            "_id": "pants_baggy_black_female",
             "name": "pants_baggy_black_female.glb",
             "display_name": "Baggy Black Pants",
             "user_id": "default",
@@ -108,7 +108,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_baggy_denim_female",
+            "_id": "pants_baggy_denim_female",
             "name": "pants_baggy_denim_female.glb",
             "display_name": "Baggy Denim Pants",
             "user_id": "default",
@@ -118,7 +118,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_baggy_camo_female",
+            "_id": "pants_baggy_camo_female",
             "name": "pants_baggy_camo_female.glb",
             "display_name": "Baggy Camo Pants",
             "user_id": "default",
@@ -128,7 +128,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_leggings_female",
+            "_id": "pants_leggings_female",
             "name": "pants_leggings_female.glb",
             "display_name": "Leggings",
             "user_id": "default",
@@ -138,7 +138,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_shorts_black_female",
+            "_id": "pants_shorts_black_female",
             "name": "pants_shorts_black_female.glb",
             "display_name": "Black Shorts",
             "user_id": "default",
@@ -148,7 +148,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_shorts_camo_female",
+            "_id": "pants_shorts_camo_female",
             "name": "pants_shorts_camo_female.glb",
             "display_name": "Camo Shorts",
             "user_id": "default",
@@ -158,7 +158,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_black_male",
+            "_id": "pants_black_male",
             "name": "pants_black_male.glb",
             "display_name": "Black Pants",
             "user_id": "default",
@@ -168,7 +168,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_denim_male",
+            "_id": "pants_denim_male",
             "name": "pants_denim_male.glb",
             "display_name": "Denim Pants",
             "user_id": "default",
@@ -178,7 +178,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_camo_male",
+            "_id": "pants_camo_male",
             "name": "pants_camo_male.glb",
             "display_name": "Camo Pants",
             "user_id": "default",
@@ -188,7 +188,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_shorts_black_male",
+            "_id": "pants_shorts_black_male",
             "name": "pants_shorts_black_male.glb",
             "display_name": "Black Shorts",
             "user_id": "default",
@@ -198,7 +198,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_shorts_denim_male",
+            "_id": "pants_shorts_denim_male",
             "name": "pants_shorts_denim_male.glb",
             "display_name": "Denim Shorts",
             "user_id": "default",
@@ -208,7 +208,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "pants",
-            "id": "pants_shorts_camo_male",
+            "_id": "pants_shorts_camo_male",
             "name": "pants_shorts_camo_male.glb",
             "display_name": "Camo Shorts",
             "user_id": "default",
@@ -222,7 +222,7 @@ def _seed_default_garments(db):
     skirts = [
         {
             "type": "skirt",
-            "id": "skirt_long_lace_female",
+            "_id": "skirt_long_lace_female",
             "name": "skirt_long_lace_female.glb",
             "display_name": "Long Lace Skirt",
             "user_id": "default",
@@ -232,7 +232,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "skirt",
-            "id": "skirt_long_white_female",
+            "_id": "skirt_long_white_female",
             "name": "skirt_long_white_female.glb",
             "display_name": "Long White Skirt",
             "user_id": "default",
@@ -242,7 +242,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "skirt",
-            "id": "skirt_aline_black_female",
+            "_id": "skirt_aline_black_female",
             "name": "skirt_aline_black_female.glb",
             "display_name": "A-line Black Skirt",
             "user_id": "default",
@@ -252,7 +252,7 @@ def _seed_default_garments(db):
         },
         {
             "type": "skirt",
-            "id": "skirt_short_black_female",
+            "_id": "skirt_short_black_female",
             "name": "skirt_short_black_female.glb",
             "display_name": "Short Black Skirt",
             "user_id": "default",
@@ -266,7 +266,7 @@ def _seed_default_garments(db):
     accessories = [
         {
             "type": "accessory",
-            "id": "accessories_skirt_lace_female",
+            "_id": "accessories_skirt_lace_female",
             "name": "accessories_skirt_lace_female.glb",
             "display_name": "Lace Skirt Accessory",
             "user_id": "default",
@@ -278,16 +278,14 @@ def _seed_default_garments(db):
 
     # Seed all garments
     all_garments = shirts + pants + skirts + accessories
-    collection = db.garment_default
 
     for garment in all_garments:
-        collection.update_one(
-            {"id": garment["id"]},
+        db.garments.update_one(
+            {"_id": garment["_id"]},
             {
                 "$set": {
                     **garment,
                     "default": True,
-                    "is_dummy": True,
                     "updated_at": now,
                 },
                 "$setOnInsert": {
@@ -318,10 +316,10 @@ def seed_default_garments():
         )
         db = client[db_name]
         
-        # Create indexes on garment_default for faster queries
-        db.garment_default.create_index([('user_id', ASCENDING)])
-        db.garment_default.create_index([('default', ASCENDING)])
-        db.garment_default.create_index([('id', ASCENDING)], unique=True)
+        # Create indexes on garment for faster queries
+        db.garments.create_index([('user_id', ASCENDING)])
+        db.garments.create_index([('default', ASCENDING)])
+        db.garments.create_index([('._id', ASCENDING)], unique=True)
         
         _seed_default_garments(db)
         print("✓ Database seeding completed successfully!")
